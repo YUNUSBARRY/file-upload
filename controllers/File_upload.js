0f9c5c6e6650af09b4c6370a1a036cb88504f057
YUNUSBARRY <140173410+YUNUSBARRY@users.noreply.github.com>
2026-06-26 18:23:19 +0000

    Add createProduct function to handle product uploads

--- controllers/File_upload.js
+++ controllers/File_upload.js
@@ -1,1 +1,6 @@
+const Product = require("../../models/products")
 
+const createProduct = async(req, res) => {
+  const products = await products.find({})
+  res.status(StatusCodes.OK).json({count: products.length, products})
+}
